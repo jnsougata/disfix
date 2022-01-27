3261643adeda0a4c5cae1e2bb5eae7d2806ea0fb
Zen <jnsougata@gmail.com>
2022-01-27 11:01:16 +0530

    modified

--- src/extslash/client.py
+++ src/extslash/client.py
@@ -8,7 +8,8 @@ from .type import Slash
 from dataclasses import dataclass
 from discord.http import Route
 from functools import wraps
-from typing import Callable, Optional, Any, Union, List
+from discord.utils import _to_json
+from typing import Callable, Optional, Any, Union, List, Sequence, Iterable
 
 
 @dataclass(frozen=True)
@@ -111,51 +112,109 @@ class SlashContext:
             user_id = self._interaction.user.get('id')
             return self._client.get_user(int(user_id))
 
+    async def respond(
+            self,
+            content: Union[str, Any] = None,
+            *,
+            tts: bool = False,
+            file: Optional[discord.File] = None,
+            files: Sequence[discord.File] = None,
+            embed: Optional[discord.Embed] = None,
+            embeds: Optional[Iterable[Optional[discord.Embed]]] = None,
+            allowed_mentions: Optional[discord.AllowedMentions] = None,
+            components: Optional[List[discord.Component]] = None,
+            ephemeral: bool = False
+    ):
+        form = []
+        route = Route('POST', f'/interactions/{self._interaction.id}/{self._interaction.token}/callback')
+
+        payload: Dict[str, Any] = {'tts': tts}
+        if content:
+            payload['content'] = content
+        if embed:
+            payload['embeds'] = [embed]
+        if embeds:
+            payload['embeds'] = embeds
+        if allowed_mentions:
+            payload['allowed_mentions'] = allowed_mentions
+        if components:
+            payload['components'] = components
+        if ephemeral:
+            payload['flags'] = 64
+        if file:
+            files = [file]
+        if not files:
+            files = []
+
+        # handling non-attachment data
+        form.append(
+            {
+                'name': 'payload_json',
+                'value': json.dumps({
+                    'type': 4,
+                    'data': json.loads(_to_json(payload))
+                })
+            }
+        )
+
+        # handling attachment data
+        if len(files) == 1:
+            file = files[0]
+            form.append(
+                {
+                    'name': 'file',
+                    'value': file.fp,
+                    'filename': file.filename,
+                    'content_type': 'application/octet-stream',
+                }
+            )
+        else:
+            for index, file in enumerate(files):
+                form.append(
+                    {
+                        'name': f'file{index}',
+                        'value': file.fp,
+                        'filename': file.filename,
+                        'content_type': 'application/octet-stream',
+                    }
+                )
+        return await self._client.http.request(route, form=form, files=files)
+
     async def send(
             self,
-            content: Union[str, Any] = None,
-            embed: discord.Embed = None,
-            embeds: List[discord.Embed] = None,
-            ephemeral: bool = False
+            content: Optional[str] = None,
+            *,
+            file: Optional[discord.File] = None,
+            files: Sequence[discord.File] = None,
+            tts: bool = False,
+            embed: Optional[discord.Embed] = None,
+            embeds: Optional[Iterable[Optional[discord.Embed]]] = None,
+            nonce: Optional[str] = None,
+            allowed_mentions: Optional[discord.AllowedMentions] = None,
+            message_reference: Optional[discord.MessageReference] = None,
+            stickers: Optional[List[discord.StickerItem]] = None,
+            components: Optional[List[discord.Component]] = None,
     ):
-        if embed:
-            payload = [embed.to_dict()]
-        elif embeds:
-            payload = [embed.to_dict() for embed in embeds]
-        else:
-            payload = []
-
-        route = Route('POST', f'/channels/{self._interaction.channel_id}/messages')
-        body = {
-            "content": str(content) if content else ' ',
-            "embeds": payload,
-            "flags": 64 if ephemeral else None
-        }
-        return await self._client.http.request(route, json=body)
-
-    async def reply(
-            self,
-            content: Union[str, Any] = None,
-            embed: discord.Embed = None,
-            embeds: List[discord.Embed] = None,
-            ephemeral: bool = False
-    ):
-        if embed:
-            payload = [embed.to_dict()]
-        elif embeds:
-            payload = [embed.to_dict() for embed in embeds]
-        else:
-            payload = []
-        route = Route('POST', f'/interactions/{self._interaction.id}/{self._interaction.token}/callback')
-        body = {
-            'type': 4,
-            'data': {
-                "content": str(content) if content else ' ',
-                "embeds": payload,
-                "flags": 64 if ephemeral else None,
-            }
-        }
-        return await self._client.http.request(route, json=body)
+        if file:
+            files = [file]
+        if not files:
+            files = []
+        ack_route = Route('POST', f'/interactions/{self._interaction.id}/{self._interaction.token}/callback')
+        channel_route = Route('POST', f'/channels/{self.channel.id}/messages')
+        await self._client.http.request(ack_route, json={'type': 4, 'data': {'content': '\u200e', 'ephemeral': True}})
+        return await self._client.http.send_multipart_helper(
+            route=channel_route,
+            content=content,
+            files=files,
+            tts=tts,
+            embed=embed,
+            embeds=embeds,
+            nonce=nonce,
+            allowed_mentions=allowed_mentions,
+            message_reference=message_reference,
+            stickers=stickers,
+            components=components,
+        )
 
 
 class SlashBot(commands.Bot):
