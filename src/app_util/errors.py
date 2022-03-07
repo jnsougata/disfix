1e85b5d5b50d78e7455126422617b9672f8977a9
Zen <jnsougata@gmail.com>
2022-03-08 00:24:57 +0530

    enhanced

--- src/app_util/errors.py
+++ src/app_util/errors.py
@@ -23,21 +23,6 @@ class CommandNotImplemented(Exception):
         super().__init__(message)
 
 
-class NoGuildProvided(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
-
-
 class TypeMismatch(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
-
-
-class NameMismatch(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
-
-
-class NameAlreadyExists(Exception):
     def __init__(self, message: str):
         super().__init__(message)
