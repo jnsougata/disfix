de6d45634658d4a32f06192bd45eb91088ca5a28
Zen <jnsougata@gmail.com>
2022-03-07 19:37:47 +0530

    bug fix

--- setup.py
+++ setup.py
@@ -17,9 +17,6 @@ setup(
     author_email='jnsougata@gmail.com',
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         "Programming Language :: Python :: 3.10",
@@ -28,7 +25,7 @@ setup(
     keywords='discord.py, slash-commands, application-commands, discord-bot',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    python_requires='>=3.6, <4',
+    python_requires='>=3.8',
     install_requires=[],
     project_urls={
         'Bug Reports': 'https://github.com/jnsougata/app_util/issues',
